8f173431670fb25ab0c93d79b69329e74b04c475
Fabricio Masiero <masiero572@gmail.com>
2017-07-31 17:38:58 -0300

    Example with error

--- CoachMarksExample/ViewController.swift
+++ CoachMarksExample/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import CoachMarks
+
 
 class ViewController: UIViewController {
+    
+    private lazy var coachmark: CoachmarkView = CoachmarkView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.navigationController?.view.addSubview(coachmark)
+        coachmark.translatesAutoresizingMaskIntoConstraints = false
+        coachmark.leftAnchor.constraint(equalTo: self.navigationController!.view.leftAnchor).isActive = true
+        coachmark.topAnchor.constraint(equalTo: self.navigationController!.view.topAnchor).isActive = true
+        coachmark.bottomAnchor.constraint(equalTo: self.navigationController!.view.bottomAnchor).isActive = true
+        coachmark.rightAnchor.constraint(equalTo: self.navigationController!.view.rightAnchor).isActive = true
+        
+        
+        coachmark.textLabel.textColor = .white
+        coachmark.textLabel.text = "👋 this is a little example app showcasing some coach marks"
+        
+        coachmark.present(in: (self.navigationController?.view)!, from: CGRect.init(x: 332, y: 30, width: 30, height: 30).insetBy(dx: -3, dy: -3), focusingOnElement: true)
     }
 
     override func didReceiveMemoryWarning() {
